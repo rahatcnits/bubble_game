bfb8d68c6b4315df5a49caaf85e699b4bcddffdc
rahatcnits <rahatcnits@gmail.com>
2023-10-11 19:02:45 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+function makeBubble() {
+  var clutter = "";
+
+  for (let i = 1; i <= 168; i++) {
+    var rn = Math.floor(Math.random() * 10);
+    clutter += `<div class="bubble">${rn}</div>`;
+  }
+
+  document.querySelector("#pbtm").innerHTML = clutter;
+}
+
+makeBubble();
